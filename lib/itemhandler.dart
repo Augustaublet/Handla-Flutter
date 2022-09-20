0ef520ef7eb38939d9e6c59e19ae9931e38eabeb
August Aublet <august.aublet@gmail.com>
2022-09-20 12:13:00 +0200

    ny fil för item class

--- lib/itemhandler.dart
+++ lib/itemhandler.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import './itemclass.dart';
 
 class ItemHandler extends ChangeNotifier {
   List<Item> _items = [];
@@ -64,34 +65,5 @@ class ItemHandler extends ChangeNotifier {
       ));
     });
     return newList;
-  }
-}
-
-class Item {
-  var _name;
-  var _isDone;
-  var _id;
-  Item({String name = "", String id = "", isDone = false}) {
-    this._name = name;
-    this._isDone = isDone;
-    this._id = id;
-  }
-
-  String get name => _name;
-  bool get isDone => _isDone;
-  String get id => _id;
-
-  set name(String newName) {
-    _name = newName;
-  }
-
-  setIsDone() {
-    _isDone = !_isDone;
-  }
-
-  // endast för att testa så classen byggts korrekt
-  testPrint() {
-    print("Item name: $_name");
-    print("Item is done: $_isDone");
   }
 }
